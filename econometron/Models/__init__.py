22e18dfecee0f3ee0c8b3ed595d71643aea64784
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-06-12 16:47:04 +0100

    added N-beats model

--- econometron/Models/__init__.py
+++ econometron/Models/__init__.py
@@ -1,5 +1,7 @@
 from econometron.Models.Linear_RE import RE_model
 from econometron.Models.Base_Var import VAR
 from econometron.Models.varima import VARIMA
+from econometron.Models.n_beats import *
+from econometron.Models.nonlinear_RE import nonLinear_RE
 
-__all__ = ['RE_model', 'VAR' ,'VARIMA']
+__all__ = ['RE_model', 'VAR', 'VARIMA', 'n_beats', 'nonLinear_RE']
