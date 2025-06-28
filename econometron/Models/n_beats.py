1476ebf7bdba7bb0b7135091f5f7944d72072227
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-06-28 15:23:58 +0100

    updating The N_Beats Model

--- econometron/Models/n_beats.py
+++ econometron/Models/n_beats.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingLR
 import copy
 
+
 class generic_basis(nn.Module):
     def __init__(self,backcast_length,forecast_length):
       super(generic_basis, self).__init__()
@@ -28,7 +29,7 @@ class generic_basis(nn.Module):
       print("basis_b shape:", self.basis_b.shape)
       backcast=torch.matmul(theta_b,self.basis_b)+self.b_b
       forecast=torch.matmul(theta_f,self.basis_f)+self.b_f
-      return backcast,forecast
+      return forecast,backcast
 class polynomial_basis(nn.Module):
     def __init__(self, degree, backcast_length, forecast_length):
         super(polynomial_basis, self).__init__()
@@ -41,14 +42,13 @@ class polynomial_basis(nn.Module):
         #so the vector of poly is the time index
         T_forecast_prime = np.linspace(0, 1, forecast_length + 1)[:-1]
         T_backcast_prime = np.linspace(0, 1, backcast_length + 1)[:-1]
-        basis_forecast = np.zeros((degree + 1, forecast_length))  # Changed: (degree+1, forecast_length)
-        basis_backcast = np.zeros((degree + 1, backcast_length))  # Changed: (degree+1, backcast_length)
+        basis_forecast = np.zeros((degree + 1, forecast_length))  # keep this as (degree+1, forecast_length)
+        basis_backcast = np.zeros((degree + 1, backcast_length))  # keep this as (degree+1, backcast_length)
         for i in range(degree + 1):
             basis_forecast[i, :] = T_forecast_prime ** i
             basis_backcast[i, :] = T_backcast_prime ** i
         self.register_buffer('forecast_basis', torch.tensor(basis_forecast, dtype=torch.float32))
         self.register_buffer('backcast_basis', torch.tensor(basis_backcast, dtype=torch.float32))
-
     def forward(self, theta_b, theta_f):
         print("theta_b shape:", theta_b.shape)
         print("theta_f shape:", theta_f.shape)
@@ -90,9 +90,9 @@ class chebyshev_basis(nn.Module):
         self.register_buffer('forecast_basis', torch.tensor(basis_fore, dtype=torch.float32))
         self.register_buffer('backcast_basis', torch.tensor(basis_back, dtype=torch.float32))
       def forward(self, theta_b,theta_f):
-          forecast = torch.matmul(theta_b,self.forecast_basis)
-          backcast = torch.matmul(theta_f,self.backcast_basis)
-          return backcast, forecast
+          forecast = torch.matmul(theta_f,self.forecast_basis)
+          backcast = torch.matmul(theta_b,self.backcast_basis)
+          return forecast,backcast
 class fourier_basis(nn.Module):
     def __init__(self, backcast_length, forecast_length):
         super(fourier_basis, self).__init__()
@@ -118,68 +118,174 @@ class fourier_basis(nn.Module):
             basis_fore[2*(l-1)+1, :] = np.sin(2 * np.pi * l * t_fore)    
         self.register_buffer('backcast_basis', torch.FloatTensor(basis_back))
         self.register_buffer('forecast_basis', torch.FloatTensor(basis_fore))
-    
     def forward(self, theta_b, theta_f):
         forecast = torch.matmul(theta_f, self.forecast_basis)
         backcast = torch.matmul(theta_b, self.backcast_basis)
         return forecast, backcast
-    
 class N_beats_Block(nn.Module):
-    def __init__(self,input_size,Horizon,backcast,degree,
-                    Hidden_size=512,
-                    basis_type="generic"):
-        super(N_beats_Block,self).__init__()
-        self.basis_type=basis_type
-        self.input_size=input_size
-        self.degree=degree
-        self.Horizon=Horizon
-        self.backcast=backcast
+    def __init__(self,
+                 input_size: int,
+                 Horizon: int,
+                 backcast: int,
+                 degree: int,
+                 n_layers: int,
+                 Hidden_size: int = 512,
+                 basis_type: str = "generic"):
+        super(N_beats_Block, self).__init__()
+        self.basis_type = basis_type
+        self.input_size = input_size
+        self.degree = degree
+        self.Horizon = Horizon
+        self.backcast = backcast
         #####
-        # we will set 4 layers for teh Fully connected stack 
-        self.FC_stack=nn.Sequential(
-            nn.Linear(in_features=input_size,out_features=Hidden_size),
-            nn.ReLU(),#h(l1)
-            nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
-            nn.ReLU(),#h(l2)
-            nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
-            nn.ReLU(),#h(l3)
-            nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
-            nn.ReLU() #h(l4)  
-            )
-        #see page 3 in N-BEATS paper by Boris N. Oreshkin
-        # Now we prepare for the FC layer within each basis type choice  
-        #please contact me @mohamedamine.ouerfelli@outlook.com specially 
-        #if the matter concerns the architecture of the model and the single FC layer in the Nbeats block with generates theta
-        if basis_type in ['generic','fourier','chebyshev','polynomial']:
-            self.basis=basis_type
+        # we will set 4 layers for teh Fully connected stack
+        #  self.FC_stack=nn.Sequential(
+        #      nn.Linear(in_features=input_size,out_features=Hidden_size),
+        #      nn.ReLU(),#h(l1)
+        #      nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
+        #      nn.ReLU(),#h(l2)
+        #      nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
+        #      nn.ReLU(),#h(l3)
+        #      nn.Linear(in_features=Hidden_size,out_features=Hidden_size),
+        #      nn.ReLU() #h(l4)
+        #  )
+        layers = []
+        for i in range(n_layers):
+            layers.append(nn.Linear(in_features=input_size, out_features=Hidden_size))
+            layers.append(nn.ReLU())
+            input_size = Hidden_size
+        self.FC_stack = nn.Sequential(*layers)
+        # see page 3 in N-BEATS paper by Boris N. Oreshkin
+        # Now we prepare for the FC layer within each basis type choice
+        # please contact me @mohamedamine.ouerfelli@outlook.com specially
+        # if the matter concerns the architecture of the model and the single FC layer in the Nbeats block with generates theta
+        if basis_type in ['generic', 'fourier', 'chebyshev', 'polynomial']:
+            self.basis = basis_type
         else:
             raise ValueError(f"Unknown basis type: {basis_type}")
-        if self.basis=='generic':
-            self.theta_f=nn.Linear(in_features=Hidden_size,out_features=Horizon)
-            self.theta_b=nn.Linear(in_features=Hidden_size,out_features=backcast)
-            self.basis_function=generic_basis(backcast_length=backcast,forecast_length=Horizon)
-        elif self.basis=='fourier':
-            theta_Hor=self.Horizon//2 - 1
-            theta_back=self.backcast//2 - 1
-            self.theta_f=nn.Linear(in_features=Hidden_size,out_features=2*theta_Hor)
-            self.theta_b=nn.Linear(in_features=Hidden_size,out_features=2*theta_back)
-            self.basis_function=fourier_basis(backcast_length=backcast,forecast_length=Horizon)
-        elif self.basis=='chebyshev':
-            self.theta_f=nn.Linear(in_features=Hidden_size,out_features=self.degree+1)
-            self.theta_b=nn.Linear(in_features=Hidden_size,out_features=self.degree+1)
-            self.basis_function=chebyshev_basis(backcast_length=backcast,forecast_length=Horizon,degree=degree)
-        elif self.basis=='polynomial':
-            self.theta_f=nn.Linear(in_features=Hidden_size,out_features=self.degree+1)
-            self.theta_b=nn.Linear(in_features=Hidden_size,out_features=self.degree+1)
-            self.basis_function=polynomial_basis(backcast_length=backcast,forecast_length=Horizon,degree=degree)
+        if self.basis == 'generic':
+            self.theta_f = nn.Linear(in_features=Hidden_size, out_features=Horizon)
+            self.theta_b = nn.Linear(in_features=Hidden_size, out_features=backcast)
+            self.basis_function = generic_basis(backcast_length=backcast, forecast_length=Horizon)
+        elif self.basis == 'fourier':
+            theta_Hor = self.Horizon // 2 - 1
+            theta_back = self.backcast // 2 - 1
+            self.theta_f = nn.Linear(in_features=Hidden_size, out_features=2 * theta_Hor)
+            self.theta_b = nn.Linear(in_features=Hidden_size, out_features=2 * theta_back)
+            self.basis_function = fourier_basis(backcast_length=backcast, forecast_length=Horizon)
+        elif self.basis == 'chebyshev':
+            if degree is None:
+                degree = 3
+            self.theta_f = nn.Linear(in_features=Hidden_size, out_features=self.degree + 1)
+            self.theta_b = nn.Linear(in_features=Hidden_size, out_features=self.degree + 1)
+            self.basis_function = chebyshev_basis(backcast_length=backcast, forecast_length=Horizon, degree=degree)
+        elif self.basis == 'polynomial':
+            if degree is None:
+                degree = 3
+            self.theta_f = nn.Linear(in_features=Hidden_size, out_features=self.degree + 1)
+            self.theta_b = nn.Linear(in_features=Hidden_size, out_features=self.degree + 1)
+            self.basis_function = polynomial_basis(backcast_length=backcast, forecast_length=Horizon, degree=degree)
         for m in self.modules():
             if isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
-    def forward(self,x):
-        h_4=self.FC_stack(x)
-        theta_b=self.theta_b(h_4)
-        theta_f=self.theta_f(h_4)
-        forecast,backcast=self.basis_function(theta_b,theta_f)
-        return forecast,backcast
+    def forward(self, x):
+        h_4 = self.FC_stack(x)
+        theta_b = self.theta_b(h_4)
+        theta_f = self.theta_f(h_4)
+        forecast, backcast = self.basis_function(theta_b, theta_f)
+        return forecast, backcast
+class N_beats_stack(nn.Module):
+    def __init__(self, input_size, n_blocks, horizon, backcast_size,n_layers_per_block, hidden_size,
+                 degree, basis_type="generic", share_weights=True):
+        super(N_beats_stack, self).__init__()
+        self.n_blocks = n_blocks
+        self.horizon = horizon
+        self.backcast_size = backcast_size
+        self.share_weights = share_weights
+        if share_weights and n_blocks > 0:
+            self.shared_block = N_beats_Block(
+                input_size=input_size,
+                Horizon=self.horizon, 
+                backcast=self.backcast_size,
+                degree=degree,
+                n_layers=n_layers_per_block,
+                Hidden_size=hidden_size,
+                basis_type=basis_type
+            )
+            self.blocks = nn.ModuleList([self.shared_block for _ in range(n_blocks)])
+        else:
+            self.blocks = nn.ModuleList([
+                N_beats_Block(
+                    input_size=input_size,
+                    Horizon=self.horizon, 
+                    backcast=self.backcast_size, 
+                    degree=degree,
+                    n_layers=n_layers_per_block,
+                    Hidden_size=hidden_size,
+                    basis_type=basis_type,
+                ) for _ in range(n_blocks)
+            ])
+    def forward(self, x):
+        if self.n_blocks == 0:
+            raise ValueError("Number of blocks must be greater than 0")
+        residual = x
+        stack_forecast = torch.zeros(x.shape[0], self.horizon, device=x.device)
+        print(stack_forecast.shape)
+        for block in self.blocks:
+            forecast, backcast = block(residual)
+            stack_forecast += forecast
+            residual = residual - backcast    
+        return forecast, backcast
+class N_beats(nn.Module):
+    def __init__(self, stack_configs, backcast_length, forecast_length):
+        super(N_beats, self).__init__()
+        self.backcast_length = backcast_length
+        self.forecast_length = forecast_length
+        self.stack_configs = stack_configs
+        self.stacks = nn.ModuleList()       
+        for config in stack_configs:
+            n_blocks = config['n_blocks']
+            basis_type = config['basis_type']
+            n_layers_per_block = config.get('n_layers_per_block', 4)
+            hidden_size = config.get('hidden_size', 512)
+            degree = config.get('degree', 3)
+            share_weights = config.get('share_weights', True)    
+            stack = N_beats_stack(
+                input_size=backcast_length,
+                n_blocks=n_blocks,
+                horizon=forecast_length,
+                backcast_size=backcast_length,
+                n_layers_per_block=n_layers_per_block,
+                hidden_size=hidden_size,
+                degree=degree,
+                basis_type=basis_type,
+                share_weights=share_weights)          
+            self.stacks.append(stack)    
+    def forward(self, x):
+        residual = x
+        total_forecast = torch.zeros(x.shape[0], self.forecast_length, device=x.device)
+        for stack in self.stacks:
+            stack_forecast, residual = stack(residual)
+            total_forecast += stack_forecast           
+        return total_forecast
+    def get_model_info(self):
+        info = {
+            'backcast_length': self.backcast_length,
+            'forecast_length': self.forecast_length,
+            'num_stacks': len(self.stacks),
+            'stack_configs': []
+        }     
+        for i, config in enumerate(self.stack_configs):
+            stack_info = {
+                'stack_id': i,
+                'n_blocks': config['n_blocks'],
+                'basis_type': config['basis_type'],
+                'hidden_size': config.get('hidden_size', 512),
+                'n_layers_per_block': config.get('n_layers_per_block', 4)
+            }
+            if config['basis_type'] in ['polynomial', 'chebyshev']:
+                stack_info['degree'] = config.get('degree', 3)
+            info['stack_configs'].append(stack_info)
+        return info
