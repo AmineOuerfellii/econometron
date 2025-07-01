1741465abb9c4cd8e5a0ddbc74894bdb299daa4a
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-07-01 13:51:43 +0100

    updated utils/data_preparation

--- econometron/utils/data_preparation/__init__.py
+++ econometron/utils/data_preparation/__init__.py
@@ -1,3 +1,3 @@
 from .process_timeseries import process_time_series
-from .scaler import StandardScaler, MinMaxScaler,mean_absolute_error, mean_squared_error
-__all__ = ['process_time_series','StandardScaler','MinMaxScaler','mean_absolute_error','mean_squared_error']
+from .scaler import StandardScaler, MinMaxScaler,mean_absolute_error, mean_squared_error,r2_score,root_mean_squared_error,mean_absolute_percentage_error
+__all__ = ['process_time_series','StandardScaler','MinMaxScaler','mean_absolute_error','mean_squared_error','r2_score','root_mean_squared_error','mean_absolute_percentage_error']
