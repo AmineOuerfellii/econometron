7f6887dfa5548dca85592ee59e46a4a82be88b10
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-08-28 23:29:55 +0100

    (Update#21)-State Space CLass added

--- Tutorials/Models/Linear_DSGE/test_ss.py
+++ Tutorials/Models/Linear_DSGE/test_ss.py
@@ -7,7 +7,7 @@ from econometron.Models.StateSpace import SS_Model
 from econometron.utils.data_preparation import TransformTS
 import os
 from econometron.filters import Kalman,kalman_objective
-
+from scipy.stats import gamma,beta as beta_dist
 data_dir= os.path.join(os.path.dirname(__file__), "../../Data_Fred/")
 
 GDP = pd.read_csv(os.path.join(data_dir, "gdp.csv"), index_col=0, parse_dates=True)
@@ -24,13 +24,13 @@ exo_states=['xbar']
 shock=['sigmax']
 # Parameters dictionary
 parameters = {
-    'g': 5,      # Inverse of relative risk aversion (1/g)
-    'beta': 0.99,       # Discount factor
-    'kappa': 0,
-    'rho': 0.95,        # Persistence of output gap target
-    'phi': 1.5,         # Taylor rule inflation coefficient
-    'd': 0.5,          # Calvo parameter
-    'sigmax':0.01
+'g': 7.372995153121833,
+ 'beta': 0.96,
+ 'kappa': 0.8,
+ 'rho': 0.9877384102847888,
+ 'phi': 3.402829610145752,
+ 'd': 0.003082995908878812,
+ 'sigmax': 0.006405250449062197
 }
 
 sigma_X,beta,g,rho,phi,d=parameters['sigmax'],parameters['beta'],parameters['g'],parameters['rho'],parameters['phi'],parameters['d']
@@ -62,19 +62,19 @@ TSP=TransformTS(mac_data.T,method='hp',lamb=1600)
 TSP.get_transformed_data().describe()
 trans_data=TSP.get_transformed_data().T.values
 model_full_params = {
-    'g': 1.00000000e+01,
+    'g': 5,
     'beta': 8.97384125e-01,
     'kappa': 0.8,
     'rho': 9.61923424e-01,
-    'phi': 1,
+    'phi': 4,
     'd': 8.64607398e-01 ,
-    'sigmax': 7.52359617e-03,
+    'sigmax': 0.01,
     'sigma_y': 0.01,
     'sigma_p': 0.01,
     'sigma_r': 0.01
 }
 
-Keynes_ss=SS_Model(data=trans_data,parameters=model_full_params,model=new_keynisian_model,optimizer='SA')
+Keynes_ss=SS_Model(data=trans_data,parameters=model_full_params,model=new_keynisian_model,optimizer='SA',estimation_method='Bayesian')
 def R_mat(p):
     A1 = np.array([[1, -p ['phi'], 0],
                     [0, 1, -p['kappa']],
@@ -88,7 +88,12 @@ def R_mat(p):
 def Q_mat(p):
     Q=np.array([[p['sigmax']]])
     return Q
-defined_params=({'kappa':'((1 - d) * (1 - d * beta)) / d'})
+def A_mat(p):
+    A=np.array([[p['rho']]])
+    return A
+def D_mat(p):
+    D=np.array([[]])
+defined_params=({'kappa':'((1-d)*(1-(d*beta)))/d'})
 calibrated_params=[('beta',0.96)]
 
 
@@ -97,25 +102,35 @@ Keynes_ss.set_state_cov(Q_mat)
 Keynes_ss.set_obs_cov(R_mat)
 Keynes_ss.calibrate_params(calibrated_params)
 Keynes_ss.define_parameter(defined_params)
-update_ss=Keynes_ss._make_state_space_updater(base_params=model_full_params)
-param={'g': 7.372995153121833,
- 'beta': 0.96,
- 'kappa': 0.8,
- 'rho': 0.9877384102847888,
- 'phi': 3.402829610145752,
- 'd': 0.003082995908878812,
- 'sigmax': 0.006405250449062197,
- 'sigma_y': 0.10709796645551589,
- 'sigma_p': 0.004041130660498564,
- 'sigma_r': 0.999999142653202}
-print(param['sigmax']**2)
-update_ss=Keynes_ss._make_state_space_updater(base_params=param)
-print(update_ss(params=param))
+update_ss=Keynes_ss._make_state_space_updater()
+# param={'g': 7.372995153121833,
+#  'beta': 0.97,
+#  'rho': 0.9877384102847888,
+#  'phi': 3.402829610145752,
+#  'd': 0.003082995908878812,
+#  'sigmax': 0.006405250449062197,
+#  'sigma_y': 0.10709796645551589,
+#  'sigma_p': 0.004041130660498564,
+#  'sigma_r': 0.999999142653202}
+# mll=kalman_objective(param.values(),{},param.keys(),trans_data,update_ss)
+# print(mll)
 
-ll=kalman_objective(param.values(),{},param.keys(),trans_data,update_ss)
-# LB= [0,0,1,0,0,0,0,0]
-# UB= [10,1,5,1,1,1,1,1]
-# Keynes_ss.fit(Lower_bound=LB,Upper_bound=UB,)
-# print(Keynes_ss.result)
-# Keynes_ss.summary()
+LB= [0,0,1,0,0,0,0,0]
+UB= [10,1,5,1,1,1,1,1]
+priors = {
+    'g':       (gamma, {'a': 5, 'scale': 1}),
+    'rho':     (beta_dist, {'a': 19, 'b': 1}),
+    'phi':     (gamma, {'a': 3, 'scale': 0.5}),
+    'd':       (beta_dist, {'a': 10, 'b': 10}),
+    'sigmax':  (gamma, {'a': 2, 'scale': 0.02}),
+    'sigma_y': (gamma, {'a': 2, 'scale': 0.02}),
+    'sigma_p': (gamma, {'a': 2, 'scale': 0.02}),
+    'sigma_r': (gamma, {'a': 2, 'scale': 0.02}),
+}
+sigma=[0.02,0.02, 0.01, 0.01, 0.002, 0.002, 0.002, 0.002]
+Keynes_ss.fit(Lower_bound=LB,Upper_bound=UB,prior_specs=priors,stand_div=sigma)
+print(Keynes_ss.result)
+Keynes_ss.summary()
+Keynes_ss.evaluate()
+Keynes_ss.predict()
 
