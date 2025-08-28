84c2daf5d4aada4918298d5d572ba0d9ccc64434
AmineOuerfelli <60555951+AmineOuerfellii@users.noreply.github.com>
2025-08-28 23:48:27 +0100

    (Update#24)

---
+++ econometron/Models/Neuralnets/__init__.py
@@ -0,0 +1,1 @@
+from .n_beats import NBEATS,Trainer_ts
--- econometron/Models/__init__.py
+++ econometron/Models/__init__.py
@@ -1,6 +1,6 @@
 from econometron.Models.dynamicsge import *
 from econometron.Models.VectorAutoReg import *
-from econometron.Models.Neuralnets.n_beats import *
+from econometron.Models.Neuralnets import *
 __all__ = []
 # dynamicsge
 from econometron.Models.dynamicsge import nonlinear_dsge, linear_dsge
@@ -9,7 +9,7 @@ __all__ += ['linear_dsge', 'nonlinear_dsge']
 from econometron.Models.VectorAutoReg import SVAR, VAR, VARMA ,VARIMA
 __all__ += ['SVAR','VAR','VARMA' ,'VARIMA']
 # Neuralnets
-from econometron.Models.Neuralnets.n_beats import *
-__all__ += ['n_beats']
+from econometron.Models.Neuralnets import Trainer_ts,NBEATS
+__all__ += ['NBEATS','Trainer_ts']
 from econometron.Models.StateSpace import SS_Model
 __all__ +=['SS_Model']
